7496109ad4344e7cce552aa48a6a1de89c855baa
Kartik Ahalawat <kartikahalawat01@gmail.com>
2023-03-08 13:32:18 +0530

    Typing error resolved in app.set in app.js file

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express =require('express');
 const app=express();
-
 const port =3003;
 
 const server = app.listen(port, ()=>{
     console.log("Server listening on port " + port)
 });
 
-app.set("view engine", pug );
+app.set("view engine", "pug");
 app.set("views", "views");
 
 app.get("/", (req, res, next)=>{
